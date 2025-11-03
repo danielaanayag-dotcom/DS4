8906536e155af2b10914210a75b36ed84cdcd3c9
Daniela Anaya <daniela.anayag27@gmail.com>
2025-11-03 18:59:18 -0500

    :)

---
+++ Laboratorio2/Laboratorio21/Laboratorio21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Laboratorio21
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Asignado valor a variable estatica.
+            MyClass.valor = 1;
+            Console.WriteLine(MyClass.valor);
+        }
+    }
+    public class MyClass
+    {
+        //Declarando variable estatica
+        public static int valor;
+    }
+}
