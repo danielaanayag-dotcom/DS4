6008cf790318fb27ab10128154a856a411a82245
Daniela Anaya <daniela.anayag27@gmail.com>
2025-11-03 19:21:19 -0500

    .

---
+++ Laboratorio9/Laboratorio95/Laboratorio95/Aleatorio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Aleatorio
+{
+    private Random random;
+
+    public Aleatorio()
+    {
+        random = new Random();
+    }
+
+    public int NumeroEntre(int min, int max)
+    {
+        return random.Next(min, max + 1);
+    }
+
+    public int[] ArregloEntre(int min, int max, int cantidad)
+    {
+        int[] arreglo = new int[cantidad];
+        for (int i = 0; i < cantidad; i++)
+        {
+            arreglo[i] = NumeroEntre(min, max);
+        }
+        return arreglo;
+    }
+
+    public int[] GenerarArregloNoRepetido()
+    {
+        int min = 1;
+        int max = 50;
+        int cantidad = 10;
+
+        List<int> listaNumeros = new List<int>();
+
+        while (listaNumeros.Count < cantidad)
+        {
+            int nuevoNumero = random.Next(min, max + 1);
+
+            if (!listaNumeros.Contains(nuevoNumero))
+            {
+                listaNumeros.Add(nuevoNumero);
+            }
+        }
+        return listaNumeros.ToArray();
+    }
+}
---
+++ Laboratorio9/Laboratorio95/Laboratorio95/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Aleatorio aleatorios = new Aleatorio();
+
+        int[] arregloUnico = aleatorios.GenerarArregloNoRepetido();
+
+        Console.WriteLine("Prueba del arreglo NO REPETIDO");
+        Console.WriteLine("Arreglo generado: ");
+        Console.WriteLine(string.Join(", ", arregloUnico));
+    }
+}
